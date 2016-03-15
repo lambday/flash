8ed352688bdf4d28415635f3d920a76af6ee1ef3
lambday <heavensdevil6909@gmail.com>
2016-03-15 10:27:49 +0530

    added MMD (WIP)

---
+++ src/flash/statistics/MMD.h
@@ -0,0 +1,47 @@
+/*
+ * Restructuring Shogun's statistical hypothesis testing framework.
+ * Copyright (C) 2016  Soumyajit De
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MMD_H_
+#define MMD_H_
+
+#include <flash/statistics/TwoSampleTest.h>
+
+namespace shogun
+{
+
+class CKernel;
+
+namespace statistics
+{
+
+class CMMD : public CTwoSampleTest
+{
+public:
+	CMMD();
+	virtual ~CMMD();
+
+	float64_t compute_statistic();
+//	float64_t compute_statistic(bool multiple_kernels);
+
+	virtual const char* get_name() const;
+};
+
+}
+
+}
+#endif // MMD_H_
