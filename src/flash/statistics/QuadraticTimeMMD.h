cddc45c8d4bd4a83ba08beee1d919997c3ad527d
lambday <heavensdevil6909@gmail.com>
2016-03-17 01:52:11 +0530

    added different mmd types

---
+++ src/flash/statistics/QuadraticTimeMMD.h
@@ -0,0 +1,41 @@
+/*
+ * Restructuring Shogun's statistical hypothesis testing framework.
+ * Copyright (C) 2016  Soumyajit De
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QUADRATIC_TIME_MMD_H_
+#define QUADRATIC_TIME_MMD_H_
+
+#include <flash/statistics/MMD.h>
+
+namespace shogun
+{
+
+namespace statistics
+{
+
+class CQuadraticTimeMMD : public CMMD
+{
+public:
+	CQuadraticTimeMMD();
+	virtual ~CQuadraticTimeMMD();
+	virtual const char* get_name() const;
+};
+
+}
+
+}
+#endif // QUADRATIC_TIME_MMD_H_
